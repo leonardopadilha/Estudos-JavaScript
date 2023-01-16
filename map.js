50e24d67bb8dad52c1c25d4ce440c0d3c779809c
leonardopadilha <leonardopadilha28@gmail.com>
2023-01-16 09:05:33 -0300

    Map JavaScript

---
+++ map.js
@@ -0,0 +1,16 @@
+const numeros = [1,2,3]
+const dobro = []
+
+for (let i = 0; i < numeros.length; i++) {
+    dobro.push(numeros[i] * 2)
+}
+
+console.log(numeros)
+console.log(dobro)
+
+const dobro_map = numeros.map(function(numero) {
+    return numero * 2;
+})
+
+console.log(dobro_map)
+//console.log(numeros.length)

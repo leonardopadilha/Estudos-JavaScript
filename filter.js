3da4a3a46737282d3f8962b89d2302a00bbbe118
leonardopadilha <leonardopadilha28@gmail.com>
2023-01-17 13:45:26 -0300

    filter - JavaScript

---
+++ filter.js
@@ -0,0 +1,47 @@
+const alunos = [
+    {nome: 'João', idade: 15},
+    {nome: 'José', idade: 18},
+    {nome: 'Maria', idade: 20},
+    {nome: 'Carlos', idade: 21}
+];
+
+const alunoMaior = [];
+
+alunos.forEach(function(aluno) {
+    if (aluno.idade >= 18){
+        alunoMaior.push(aluno)
+    }
+});
+
+console.log(alunoMaior)
+
+console.log()
+
+// --------------------- ---------------------------- ---------------------------
+
+const	alunosDeMaior	=	[];
+for(let	i =	0;	i <	alunos.length; i++)	{
+    if(alunos[i].idade >= 18)	{
+        alunosDeMaior.push(alunos[i]);
+    }
+};
+
+console.log(alunosDeMaior)
+
+console.log()
+
+// --------------------- ---------------------------- ---------------------------
+
+const alunosDeMaiorFilter = alunos.filter(function(aluno) {
+    return aluno.idade >= 20;
+});
+
+console.log(alunosDeMaiorFilter)
+
+console.log()
+
+// --------------------- ---------------------------- ---------------------------
+
+const maior_idade = alunos.filter(maior => maior.idade > 20);
+console.log(maior_idade[0].nome)
+console.log(maior_idade)

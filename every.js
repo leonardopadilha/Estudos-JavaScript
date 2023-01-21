5631b196cce05c538797288be614841eeac9c978
leonardopadilha <leonardopadilha28@gmail.com>
2023-01-21 12:30:30 -0300

    every - js

---
+++ every.js
@@ -0,0 +1,20 @@
+const alunos = [
+    {nome: 'joão', idade: 18},
+    {nome: 'maria', idade: 20},
+    {nome: 'pedro', idade: 24}
+];
+
+let todosAlunosDeMaior = true;
+for (let i = 0; i < alunos.length; i++) {
+    if (alunos[i].idade < 19) {
+        todosAlunosDeMaior = false;
+    }
+}
+
+console.log(todosAlunosDeMaior);
+
+let todosAlunosDeMaiorEvery = alunos.every(function(maior_idade) {
+    return maior_idade.idade >= 18;
+});
+
+console.log(todosAlunosDeMaiorEvery);

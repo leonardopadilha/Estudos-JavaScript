67e2afca462dbbea083b77fe2c43bf3d3fe63901
leonardopadilha <leonardopadilha28@gmail.com>
2023-01-30 06:03:51 -0300

    Switch js

---
+++ switch.js
@@ -0,0 +1,13 @@
+let letra = "5";
+
+switch(letra) {
+    case "a" : 
+        console.log(`A letra é ${letra}`);
+        break;
+    case "b" : 
+        console.log(`A letra é ${letra}`);
+        break;
+    default :
+        console.log("Valor inválido");
+        break;
+}
